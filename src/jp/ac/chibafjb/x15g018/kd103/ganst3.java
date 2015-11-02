2fc36a54515669d63552ba587f90345146295c0d
unknown <x15g018@PC504N010.chiba-fjb.ac.jp>
2015-11-02 14:46:57 +0900

    tekito3

---
+++ src/jp/ac/chibafjb/x15g018/kd103/ganst3.java
@@ -0,0 +1,20 @@
+package jp.ac.chibafjb.x15g018.kd103;
+
+public class ganst3 {
+
+	public static void main(String[] args) {
+		System.out.print("1 船橋 太郎");
+		System.out.print("2 船橋 太郎");
+		System.out.print("3 船橋 太郎");
+		System.out.print("4 船橋 太郎");
+		System.out.print("5 船橋 太郎");
+		System.out.print("6 船橋 太郎");
+		System.out.print("7 船橋 太郎");
+		System.out.print("8 船橋 太郎");
+		System.out.print("9 船橋 太郎");
+		System.out.print("10 船橋 太郎");
+
+
+	}
+
+}
