1b65dd843a22aa197b70c1b44bd8ca67795f7e1e
unknown <x15g018@PC504N010.chiba-fjb.ac.jp>
2015-11-09 15:07:52 +0900

    tekito8

---
+++ src/jp/ac/chibafjb/x15g018/kd035/Gunst020.java
@@ -0,0 +1,17 @@
+package jp.ac.chibafjb.x15g018.kd035;
+
+import java.util.Scanner;
+
+public class Gunst020 {
+
+	public static void main(String[] args) {
+		Scanner sin = new Scanner(System.in);
+		String s1 = sin.next();
+		String s2 = sin.next();
+		sin.close();                          //入力を閉じる 
+		System.out.println("str1 = "+s1);
+		System.out.println("str2 = "+s2);
+
+	}
+
+}
